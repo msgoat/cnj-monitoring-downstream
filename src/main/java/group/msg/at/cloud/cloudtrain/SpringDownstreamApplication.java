feb779f01278edd26a81936943987bfdb8f520f2
Michael Theis (msg) <michael.theis@msg.group>
2023-06-10 18:15:46 +0200

    removed obsolete parameters from @SpringBootApplication

--- src/main/java/group/msg/at/cloud/cloudtrain/SpringDownstreamApplication.java
+++ src/main/java/group/msg/at/cloud/cloudtrain/SpringDownstreamApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 /**
  * Point of entry for this Spring Boot application.
  */
-@SpringBootApplication(scanBasePackageClasses = {SpringDownstreamApplication.class, CommonObservability.class})
+@SpringBootApplication
 public class SpringDownstreamApplication {
 
     public static void main(String[] args) {
