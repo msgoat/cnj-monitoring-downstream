1b6f8a040bd50ca9a42003ef7b7dd1b25f749a09
Michael Theis (msg) <michael.theis@msg.group>
2024-04-03 18:04:01 +0200

    Released version 5.0.0 with major upgrades

--- src/main/java/group/msg/at/cloud/cloudtrain/SpringDownstreamApplication.java
+++ src/main/java/group/msg/at/cloud/cloudtrain/SpringDownstreamApplication.java
@@ -1,6 +1,5 @@
 package group.msg.at.cloud.cloudtrain;
 
-import group.msg.at.cloud.common.observability.CommonObservability;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
